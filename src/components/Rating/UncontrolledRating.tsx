51dba14774aca3a4dda8376c3ee22de1f9dab35e
acronisv <acronisv@gmail.com>
2022-08-09 01:37:43 +0300

    Refactor UncontrolledRating

--- src/components/Rating/UncontrolledRating.tsx
+++ src/components/Rating/UncontrolledRating.tsx
@@ -4,26 +4,33 @@ export function UncontrolledRating() {
 
     const [value, setValue] = useState(0)
 
+    const setValueCallback = (val:number) => {
+        console.log(val)
+        setValue(val)
+    }
+
     return (
         <div>
-            <Star selected={value > 0}/> <button onClick={()=>setValue(1)}>1</button>
-            <Star selected={value > 1}/> <button onClick={()=>setValue(2)}>2</button>
-            <Star selected={value > 2}/> <button onClick={()=>setValue(3)}>3</button>
-            <Star selected={value > 3}/> <button onClick={()=>setValue(4)}>4</button>
-            <Star selected={value > 4}/> <button onClick={()=>setValue(5)}>5</button>
+            <Star selected={value > 0} setValueCallback={()=>setValueCallback(1)}/>
+            <Star selected={value > 1} setValueCallback={()=>setValueCallback(2)}/>
+            <Star selected={value > 2} setValueCallback={()=>setValueCallback(3)}/>
+            <Star selected={value > 3} setValueCallback={()=>setValueCallback(4)}/>
+            <Star selected={value > 4} setValueCallback={()=>setValueCallback(5)}/>
         </div>
     )
 }
 
 
 type StarPropsType = {
-    selected: boolean
+    selected: boolean,
+    setValueCallback: ()=>void
 }
 
 function Star(props: StarPropsType) {
-    console.log("Star rendering")
-    if (props.selected === true) {
-        return <span><b>star</b> </span>
-    } else
-        return <span>star </span>
+    return (
+        <span onClick={()=> {props.setValueCallback()}}>
+            {props.selected ? <b>star </b>: <>star </>}
+        </span>
+    )
 }
+
