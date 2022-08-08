9d0e940eb602afe8234482d385144474c0504628
acronisv <acronisv@gmail.com>
2022-08-09 01:13:02 +0300

    Refactor UncontrolledRating

--- src/components/Rating/UncontrolledRating.tsx
+++ src/components/Rating/UncontrolledRating.tsx
@@ -4,17 +4,13 @@ export function UncontrolledRating() {
 
     const [value, setValue] = useState(0)
 
-    const setRating = (val:number) => {
-        setValue(val)
-    }
-
     return (
         <div>
-            <Star selected={value > 0}/> <button onClick={()=>setRating(1)}>1</button>
-            <Star selected={value > 1}/> <button onClick={()=>setRating(2)}>2</button>
-            <Star selected={value > 2}/> <button onClick={()=>setRating(3)}>3</button>
-            <Star selected={value > 3}/> <button onClick={()=>setRating(4)}>4</button>
-            <Star selected={value > 4}/> <button onClick={()=>setRating(5)}>5</button>
+            <Star selected={value > 0}/> <button onClick={()=>setValue(1)}>1</button>
+            <Star selected={value > 1}/> <button onClick={()=>setValue(2)}>2</button>
+            <Star selected={value > 2}/> <button onClick={()=>setValue(3)}>3</button>
+            <Star selected={value > 3}/> <button onClick={()=>setValue(4)}>4</button>
+            <Star selected={value > 4}/> <button onClick={()=>setValue(5)}>5</button>
         </div>
     )
 }
